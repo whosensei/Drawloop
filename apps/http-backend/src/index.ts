4a71359750a308678c7e62c7bfc206ea36ae52d7
whosensei <sukunagojo965@gmail.com>
2025-03-03 23:31:41 +0530

    "signin endpoint implemented"

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,54 +1,80 @@
 import express, { Request, Response } from "express";
-import {SigninSchema, SignupSchema} from "@repo/common/zod";
+import { SigninSchema, SignupSchema } from "@repo/common/zod";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import jwt from "jsonwebtoken";
 import { db } from "@repo/db";
 import { User } from "@repo/db/schema";
+import { eq } from "drizzle-orm";
 
 const app = express();
 
 app.use(express.json());
 
-
 app.post("/signup", async (req: Request, res: Response) => {
-    const parsedData = SignupSchema.safeParse(req.body);
-    if (!parsedData.success) {
-        console.log(parsedData.error);
-        res.status(400).json({
-            message: "Invalid credentials"
-        });
-        return;
+  const parsedData = SignupSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    console.log(parsedData.error);
+    res.status(400).json({
+      message: "Invalid credentials",
+    });
+    return;
+  }
+  try {
+    await db.insert(User).values({
+      email: parsedData.data.email,
+      username: parsedData.data.username,
+      password: parsedData.data.password,
+    });
+    res.status(200).json({
+      message: "User successfully signed up",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/signin", async (req: Request, res: Response) => {
+  const parsedData = SigninSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    console.log(parsedData.error);
+    res.status(400).json({
+      message: "invalid format",
+    });
+    return;
+  }
+
+  try {
+    const user = await db.query.User.findFirst({
+      where: eq(User.email, parsedData.data.email),
+    });
+
+    console.log("The email found is ", user?.email);
+
+    if (!user) {
+      res.status(400).json({
+        message: "User not found",
+      });
     }
-    try {
-        await db.insert(User).values({
-            email: parsedData.data.email,
-            username: parsedData.data.username,
-            password: parsedData.data.password
-        });
-        res.status(200).json({
-           message: "User successfully signed up"
-       });
-        
-    } catch (error) {
-        console.log(error)
+    if (user?.password! == parsedData.data.password) {
+      res.status(400).json({
+        message: "Incorrect password",
+      });
     }
+
+    const token = jwt.sign(parsedData.data.email, JWT_SECRET);
+    console.log(token);
+
+    res.status(200).json({
+      message: "User sucessfully signed up",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.get("/hello",(req,res)=>{
-    res.json({
-        message:"hello world"
-    }) 
+app.listen(3002, () => {
+  console.log("server started on port 3002");
 });
-app.listen(3002,()=>
-{
-    console.log("server started on port 3002")
-});
-
-
-
-
-
-
 
 // app.post("signin",(req,res)=>{
 //     const ParsedData = SigninSchema.safeParse(req.body);
@@ -62,12 +88,11 @@ app.listen(3002,()=>
 
 //     //db call to check
 //     //if exists then find the user id and generate the token
-    
+
 //     // const token = jwt.sign(UserId , process.envv.JWT_SECRET);
 //     res.json({
 //         message:"user successfully signed up"
 //     })
-
 
 // })
 
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,11 +1,14 @@
-import { neon } from '@neondatabase/serverless';
+import { neon, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as dotenv from "dotenv";
-import { resolve } from "path";
+import * as schema from "./schema";
+neonConfig.fetchConnectionCache = true 
+
 
 // Load environment variables from the root .env file
 dotenv.config({ path : "../../.env"}) ;
 
 const sql = neon(process.env.DATABASE_URL ?? "");
-export const db = drizzle({ client: sql });
+export const db = drizzle(sql, {schema});
+// export const db = drizzle(sql);
 
