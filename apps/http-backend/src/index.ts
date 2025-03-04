bf45dac4843dda16913e9d291363d0c55bfffb54
whosensei <sukunagojo965@gmail.com>
2025-03-04 22:22:29 +0530

    "signin endpoint implemented"

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -5,33 +5,9 @@ import jwt from "jsonwebtoken";
 import { db } from "@repo/db";
 import { User } from "@repo/db/schema";
 import { eq } from "drizzle-orm";
-
 const app = express();
 
 app.use(express.json());
-
-app.post("/signup", async (req: Request, res: Response) => {
-  const parsedData = SignupSchema.safeParse(req.body);
-  if (!parsedData.success) {
-    console.log(parsedData.error);
-    res.status(400).json({
-      message: "Invalid credentials",
-    });
-    return;
-  }
-  try {
-    await db.insert(User).values({
-      email: parsedData.data.email,
-      username: parsedData.data.username,
-      password: parsedData.data.password,
-    });
-    res.status(200).json({
-      message: "User successfully signed up",
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
 
 app.post("/signin", async (req: Request, res: Response) => {
   const parsedData = SigninSchema.safeParse(req.body);
@@ -42,7 +18,6 @@ app.post("/signin", async (req: Request, res: Response) => {
     });
     return;
   }
-
   try {
     const user = await db.query.User.findFirst({
       where: eq(User.email, parsedData.data.email),
@@ -55,12 +30,14 @@ app.post("/signin", async (req: Request, res: Response) => {
         message: "User not found",
       });
     }
-    if (user?.password! == parsedData.data.password) {
+    console.log(user?.password);
+    console.log(parsedData.data.password);
+
+    if (user?.password !== parsedData.data.password) {
       res.status(400).json({
         message: "Incorrect password",
       });
     }
-
     const token = jwt.sign(parsedData.data.email, JWT_SECRET);
     console.log(token);
 
@@ -76,26 +53,29 @@ app.listen(3002, () => {
   console.log("server started on port 3002");
 });
 
-// app.post("signin",(req,res)=>{
-//     const ParsedData = SigninSchema.safeParse(req.body);
-//     if(!ParsedData.success){
-//         console.log(ParsedData.error);
-//         res.status(400).json({
-//             message:"invalid credentials"
-//         })
-//         return;
-//     }
-
-//     //db call to check
-//     //if exists then find the user id and generate the token
-
-//     // const token = jwt.sign(UserId , process.envv.JWT_SECRET);
-//     res.json({
-//         message:"user successfully signed up"
-//     })
-
-// })
-
 // // app.post("create-room",middleware,(req,res)=>{
 
 // // })
+
+app.post("/signup", async (req: Request, res: Response) => {
+    const parsedData = SignupSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        console.log(parsedData.error);
+        res.status(400).json({
+            message: "Invalid credentials",
+        });
+        return;
+    }
+    try {
+        await db.insert(User).values({
+            email: parsedData.data.email,
+            username: parsedData.data.username,
+            password: parsedData.data.password,
+        });
+        res.status(200).json({
+            message: "User successfully signed up",
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,14 +1,12 @@
 import { neon, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as dotenv from "dotenv";
-import * as schema from "./schema";
+import * as schema from "./schema.ts";
 neonConfig.fetchConnectionCache = true 
-
 
 // Load environment variables from the root .env file
 dotenv.config({ path : "../../.env"}) ;
 
 const sql = neon(process.env.DATABASE_URL ?? "");
 export const db = drizzle(sql, {schema});
-// export const db = drizzle(sql);
 
