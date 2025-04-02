5aa1896dfb0b22bedbcf57b45b9411928a936390
whosensei <sukunagojo965@gmail.com>
2025-04-03 00:36:37 +0530

    Canvas functionality updated

--- apps/frontend/app/draw/index.ts
+++ apps/frontend/app/draw/index.ts
@@ -1,39 +1,81 @@
-export function initDraw(canvas : HTMLCanvasElement){
+import { Sree_Krushnadevaraya } from "next/font/google";
+import { types } from "util";
+
+export function initDraw(canvas: HTMLCanvasElement) {
 
     const ctx = canvas.getContext("2d");
-    
-    if(!ctx){
+
+    if (!ctx) {
         return
     }
-    ctx.fillStyle = "rgba(0,0,0)"
-    ctx.fillRect(0,0,canvas.width,canvas.height)
 
-    let clicked =false;
+    type shapes = {
+
+        type: "rect",
+        StartX: number,
+        StartY: number
+        width: number,
+        height: number,
+    } |
+    {
+        type: "circle",
+        radius: number,
+        startX: number,
+        startY: number
+    }
+
+
+    const ExistingShapes: shapes[] =[];
+    
+    clearCanvas(ExistingShapes,ctx,canvas)
+    
+    let clicked = false;
     let StartX: number
     let StartY: number
 
-    canvas.addEventListener("mousedown", (e)=>{
+    canvas.addEventListener("mousedown", (e) => {
         clicked = true
-        StartX =e.clientX;
+        StartX = e.clientX;
         StartY = e.clientY;
     })
 
-    canvas.addEventListener("mouseup",(e)=>{
+    canvas.addEventListener("mouseup", (e) => {
         clicked = false
         e.clientX
         e.clientY
 
+        ExistingShapes.push({
+            type:"rect",
+            StartX,
+            StartY,
+            width : e.clientX-StartX,
+            height : e.clientY-StartY
+        })
+
     })
 
-    canvas.addEventListener("mousemove",(e)=>{
-        if(clicked){
+    canvas.addEventListener("mousemove", (e) => {
+        if (clicked) {
             const width: number = e.clientX - StartX;
             const height: number = e.clientY - StartY;
-            ctx.clearRect(0,0,canvas.width,canvas.height);
-            ctx.fillStyle = "rgba(0,0,0)"
-            ctx.fillRect(0,0,canvas.width,canvas.height)
-            ctx.strokeStyle="rgba(255,255,255)"
-            ctx.strokeRect(StartX,StartY,width,height)
+            clearCanvas(ExistingShapes,ctx,canvas)
+            ctx.strokeStyle = "rgba(255,255,255)"
+            ctx.strokeRect(StartX, StartY, width, height)
         }
     })
+
+    function clearCanvas(ExistingShapes :shapes[],ctx: CanvasRenderingContext2D ,canvas :HTMLCanvasElement) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "rgba(0,0,0)"
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+        
+        ExistingShapes.map((shape)=>{
+            if(shape.type === "rect"){
+                ctx.strokeStyle = "rgba(255,255,255)"
+                ctx.strokeRect(shape.StartX,shape.StartY,shape.width,shape.height)
+            }else if(shape.type==="circle"){
+                console.log("shape")
+            }
+        })
+    }
 }
